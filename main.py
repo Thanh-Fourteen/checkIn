00e8ab74a93711fddd46d17b7c9f9e77e15fc468
Tony <thanhfourteen@gmail.com>
2024-06-28 23:53:48 +0700

    up code ver 1.0

---
+++ main.py
@@ -0,0 +1,15 @@
+from functions import functions
+import os
+
+if __name__ == "__main__":
+    folder = "D:\\FPT\\AI\\9.5 AI\\Check In\\Final1"
+    f = functions(folder)
+    f.warmup()
+
+    ## Save features in database
+    # directory = os.path.join(folder, 'img')
+    # f.process_images_from_directory(directory)
+
+    img_path = os.path.join(folder, "my_image.png")
+    f.Recognition(img_path)
+    print("Done!")
